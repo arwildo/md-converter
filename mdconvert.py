6d3fd55c1adc7f7deed15e6972a94f1904b582f0
arwildo <arwildo@gmail.com>
2019-12-03 15:57:07 +0800

    add saved file automaticly & saved_dir

--- mdconvert.py
+++ mdconvert.py
@@ -2,10 +2,12 @@
 from argparse import ArgumentParser
 
 FLAGS = None
-output_content = ""
+SAVED_DIR = "C:\My App\converted-files\/"
 
 def converter(file_content):
-    output = ("```python \n" + file_content + "\n```")
+    output_content = ""
+    output_content += ("```python \n" + file_content + "\n```")
+    return output_content
 
 
 if __name__ == '__main__':
@@ -20,9 +22,17 @@ if __name__ == '__main__':
     data_input = (str(FLAGS.f))
 
     # load file
-    with open(data_input) as file_content:
-        #call the function
-        converter(file_content)
-        print(output_content)
+    with open(data_input) as fr:
+        file_content = fr.read()
 
+    # call the function
+    converted = converter(file_content)
+
+    # save the file
+    # TODO: add regex to filter get_name for win tab
+    get_name = data_input
+    te = open(SAVED_DIR + data_input +  ".md", "x")
+    te.write(converted)
+
+    te.close()
 
